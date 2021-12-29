b406bf1d0e09fd6ae091a854b3dec582e9698caf
Ark2K <ankurravindrakushwaha@gmail.com>
2021-12-29 18:02:57 +0530

    minor optimizations

--- src/components/show/ShowMainData.jsx
+++ src/components/show/ShowMainData.jsx
@@ -4,33 +4,29 @@ import IMG_PLACEHOLDER from '../../images/not-found.png';
 import { Star } from '../styled';
 import { MainDataWrapper, Headline, TagList } from './ShowMainData.styled';
 
-const ShowMainData = ({ name, rating, summary, tags, image }) => {
-  return (
-    <MainDataWrapper>
-      <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
-      <div className="text-side">
-        <Headline>
-          <h1>{name}</h1>
-          <div>
-            <Star active />
-            <span>{rating.average || 'N/A'}</span>
-          </div>
-        </Headline>
-        <div
-          className="summary"
-          dangerouslySetInnerHTML={{ __html: summary }}
-        />
-
+const ShowMainData = ({ name, rating, summary, tags, image }) => (
+  <MainDataWrapper>
+    <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
+    <div className="text-side">
+      <Headline>
+        <h1>{name}</h1>
         <div>
-          Tags:{' '}
-          <TagList>
-            {tags.map((tag, i) => (
-              <span key={i}>{tag}</span>
-            ))}
-          </TagList>
+          <Star active />
+          <span>{rating.average || 'N/A'}</span>
         </div>
+      </Headline>
+      <div className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
+
+      <div>
+        Tags:{' '}
+        <TagList>
+          {tags.map((tag, i) => (
+            <span key={i}>{tag}</span>
+          ))}
+        </TagList>
       </div>
-    </MainDataWrapper>
-  );
-};
+    </div>
+  </MainDataWrapper>
+);
+
 export default ShowMainData;
